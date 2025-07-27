898bdd81f5a84d5895ce9a483caa838e3dd811cc
AlexMan123456 <alex.man03888@gmail.com>
2025-07-27 01:44:45 +0100

    Add initial few pages to ESLint Plugin docs

---
+++ src/pages/eslint-plugin/ESLintPluginMainPage.tsx
@@ -0,0 +1,60 @@
+import {
+  DarkModeToggle,
+  InternalLink,
+  NavigationDrawer,
+} from "@alextheman/components";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import HomeIcon from "@mui/icons-material/Home";
+import AboutPage from "src/pages/eslint-plugin/AboutPage";
+import { Outlet } from "react-router-dom";
+
+function ESLintPluginMainPage() {
+  return (
+    <NavigationDrawer
+      title="@alextheman/eslint-plugin"
+      navItems={[
+        {
+          category: "Getting Started",
+          options: [
+            {
+              label: "About",
+              to: "/eslint-plugin",
+            },
+            {
+              label: "Installation",
+              to: "/eslint-plugin/installation",
+            },
+          ],
+        },
+        {
+          category: "Rules",
+          options: [
+            {
+              label: "no-namespace-imports",
+              to: "/eslint-plugin/no-namespace-imports",
+            },
+          ],
+        },
+      ]}
+      headerElements={
+        <>
+          <Tooltip title="Return to homepage">
+            <IconButton
+              component={InternalLink}
+              to="/"
+              sx={{ display: "flex", justifySelf: "center" }}
+            >
+              <HomeIcon />
+            </IconButton>
+          </Tooltip>
+          <DarkModeToggle />
+        </>
+      }
+    >
+      {location.pathname === "/eslint-plugin" ? <AboutPage /> : <Outlet />}
+    </NavigationDrawer>
+  );
+}
+
+export default ESLintPluginMainPage;
